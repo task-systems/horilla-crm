38577e098f9fdd22c1ad945d010734e9ba2fae33
Horilla <support@horilla.com>
2026-02-04 16:00:34 +0530

    [UPDT] HORILLA: Updated core app version file into 1.1.0.

--- horilla/__version__.py
+++ horilla/__version__.py
@@ -2,8 +2,9 @@
 
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __module_name__ = _("Core System")
 __release_date__ = ""
 __description__ = ""
 __icon__ = "assets/icons/logo.png"
+__1_1_0__ = "An All Companies option has been added to the company dropdown, allowing users to view data irrespective of company selection."
