02121055d975e755f7a69fc8cc4680c8508d1925
Horilla <support@horilla.com>
2026-02-13 17:22:02 +0530

    [UPDT] DASHBOARD: Updated the version file

--- horilla_dashboard/__version__.py
+++ horilla_dashboard/__version__.py
@@ -7,8 +7,9 @@ application registry and UI.
 
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __module_name__ = "Dashboards"
 __release_date__ = ""
 __description__ = _("Module for building and customizing interactive dashboards.")
 __icon__ = "assets/icons/icon6.svg"
+__1_1_0__ = "Added drag and drop reordering from home page, date range filter, and set to default options for enhanced dashboard customization and data visualization."
