06449ccc72ea8a539f05dc52e52136527bba9d0d
Horilla <support@horilla.com>
2026-03-03 17:27:13 +0530

    [UPDT] HORILLA: Separated project URL configuration from horilla.urls to prevent circular imports and early app loading issues

---
+++ horilla/urls/project.py
@@ -0,0 +1,35 @@
+"""
+URL configuration for horilla project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.views.i18n import JavaScriptCatalog
+
+from horilla import settings
+from horilla.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
+    path("jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),
+    path("summernote/", include("django_summernote.urls")),
+    path("api/", include("horilla.api_urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
